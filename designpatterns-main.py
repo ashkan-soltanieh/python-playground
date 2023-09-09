88d3eeabf898fc8353484b33eca7ede117d416eb
Ashkan Soltanieh <ashkan98@gmail.com>
2023-09-09 14:54:16 -0600

    Use interfaces in tax calculation system

--- designpatterns-main.py
+++ designpatterns-main.py
@@ -23,3 +23,16 @@ print(user1.get_default_employer())  # output: enb
 print(user2.get_default_employer())  # output: enb
 
 # user3 = User()  # Raises TypeError because of missing required argument for constructor
+
+####################################
+### Use basics.interfaces module ###
+####################################
+from designpatterns.basics.interfaces.taxfunctions import calculate_tax
+from designpatterns.basics.interfaces.taxcalculator2021 import TaxCalculator2021
+from designpatterns.basics.interfaces.taxcalculator2022 import TaxCalculator2022
+
+tax2021 = TaxCalculator2021("tax_data")
+tax2022 = TaxCalculator2022("tax_data")
+
+print(calculate_tax(tax2021))
+print(calculate_tax(tax2022))
