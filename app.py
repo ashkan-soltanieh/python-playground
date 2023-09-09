f0ee0f4d48e33823e602e7faa75aee01c3c66b06
Ashkan Soltanieh <ashkan98@gmail.com>
2023-09-09 12:43:19 -0600

    Edit gitignore and organize existing code

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ from itertools import permutations
 
 input = "011"
 
-output = sum([list(map(list, permutations(input, i))) for i in range(len(input) + 1)], [])
+output = sum(
+    [list(map(list, permutations(input, i))) for i in range(len(input) + 1)], []
+)
 
-print(output)hi
+print(output)
